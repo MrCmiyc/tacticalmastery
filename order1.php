72c72cf024c28eb112a49576c2df264425584231
Brian Veverka <brian@adpeak.com>
2016-05-27 11:14:21 -0400

    added skeleton and validation lib to second order page

---
+++ order1.php
@@ -0,0 +1,332 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Tactical Mastery XT808</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style1.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<img src="images/logo-prim.png">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12 text-center">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title"><span class="featurehead">XT808 LED Flashlight</span></h3>
+			</div>
+			<div class="panel-body">
+				<div class="col-md-6"><img src="images/flashlight-big.jpg" class="featureimg">
+				</div>
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-md-6"><span class="featureitem">Material: Aluminum</span>
+						</div>
+						<div class="col-md-6"><span class="featureitem">Power: 3xAAA</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6"><span class="featureitem">Lumens: 800</span>
+						</div>
+						<div class="col-md-6"><span class="featureitem">Zoom: 1x-2000x</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12"><span class="featureitem">5 Modes: High | Meduim | Strobe | SOS</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12"><span class="featureitem">Normal Price: <span style="text-decoration: line-through">$116.00</span></span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+			</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<h2>
+				TODAY, YOU QUALIFY FOR FREE SHIPPING! ORDER NOW!
+			</h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<div class="panel panel-default">
+				<div class="panel-body">
+						<div class="row">
+							<div class="col-md-12 text-center">
+								<img src="images/coupon2.jpg" style="width:100%;max-width: 460px">
+								<h5>CONGRATULATIONS! YOUR FREE SHIPPING HAS BEEN APPLIED!</h5>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="col-md-12">
+								<h2>Step #1: Select Quantity</h2>
+								<hr>
+								<table cellpadding="0" cellspacing="0" class="table">
+									<thead>
+									<tr>
+									<th width="30">Item</th>
+									<th width="504"> </th>
+									<th width=53>Price</th>
+									</tr>
+									</thead>
+									<tbody>
+									<tr>
+										<td><input type="radio" name="productId" id="3lights" value="5"></td>
+										<td><label for="3lights"><strong>Buy 3 Primitive Survivor Flashlights, GET 2 FREE!</strong> ($29/EACH) <span id="txtblink" style="color:#F00; font-weight:bold;">BEST VALUE</span></label></td>
+										<td>$145.00</td>
+									</tr>
+									<tr>
+										<td><input type="radio" name="productId" value="4" id="2lights"></td>
+										<td><label for="2lights"><strong>Buy 2 Primitive Survivor Flashlights, GET 1 FREE!</strong> ($39/EACH)</label></td>
+										<td>$117.00</td>
+									</tr>
+									<tr>
+										<td><input type="radio" name="productId" value="3" id="1light"></td>
+										<td><label for="1light">1 Primitive Survivor Flashlight ($56.00)</label></td>
+										<td>$56.00</td>
+									</tr>
+									<tr>
+										<td><input type="radio" name="productId" value="6" id="4lights"></td>
+										<td><label for="4lights">2 Primitive Survivor Flashlights ($48.50 Per Unit)</label></td>
+										<td>$97.00</td>
+									</tr>
+									<tr>
+										<td><input type="radio" name="productId" value="7" id="5lights"></td>
+										<td><label for="5lights">4 Primitive Survivor Flashlights ($42.25 Per Unit)</label></td>
+										<td>$169.00</td>
+									</tr>
+									<tr>
+										<td><input type="radio" name="productId" value="8" id="10lights"></td>
+										<td><label for="10lights">10 Primitive Survivor Flashlights ($35.00 Per Unit)</label></td>
+										<td>$350.00</td>
+									</tr>
+									<tr>
+										<td><input type="radio" name="productId" value="9" id="15lights"></td>
+										<td><label for="15lights">15 Primitive Survivor Flashlights ($35.00 Per Unit)</label></td>
+										<td>$525</td>
+									</tr>
+									<tr>
+										<td><input type="radio" name="productId" value="10" id="20lights"></td>
+										<td><label for="20lights">20 Primitive Survivor Flashlights ($35.00 Per Unit)</label></td>
+										<td>$700</td>
+									</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+</div></div>
+					<div class="panel panel-default">
+						<div class="panel-body">
+
+						<div class="row">
+						<div class="col-md-12">
+							<h2>Step #2: Contact Information</h2>
+							<hr>
+							<fieldset class="form-group">
+								<label for="exampleInputEmail1">First Name</label>
+								<input type="text" class="form-control" name="firstName" placeholder="First Name" value="<?php echo $customerData['firstName']; ?>">
+							</fieldset>
+							<fieldset class="form-group">
+								<label for="exampleInputEmail1">Last Name</label>
+								<input type="text" class="form-control" name="lastName" placeholder="Last Name" value="<?php echo $customerData['lastName']; ?>">
+							</fieldset>
+							<fieldset class="form-group">
+								<label for="exampleInputEmail1">Phone Number</label>
+								<input type="text" class="form-control" name="phone" placeholder="Phone Number" value="<?php echo $customerData['phone']; ?>">
+							</fieldset>
+							<fieldset class="form-group">
+								<label for="exampleInputEmail1">Email</label>
+								<input type="email" class="form-control" name="email" placeholder="E-Mail" value="<?php echo $customerData['email']; ?>">
+								<small class="text-muted">We'll never share your email with anyone else.</small>
+							</fieldset>
+						</div></div>
+						</div>
+			</div>
+		</div>
+
+		<div class="col-md-6">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-md-12 text-center">
+							<img src="images/flashlight-big.jpg" style="width:100%;max-width: 460px">
+							<h5>Primitive Survivor Flashlight</h5>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<h2>Step #3: Shipping Address</h2>
+							<hr>
+							<fieldset class="form-group">
+								<label>Street Address</label>
+								<input type="text" class="form-control" name="address" placeholder="Your Address" value="<?php echo $customerData['address']; ?>">
+							</fieldset>
+							<fieldset class="form-group">
+								<label>City</label>
+								<input type="text" class="form-control" name="city" placeholder="Your City" value="<?php echo $customerData['city']; ?>">
+							</fieldset>
+							<fieldset class="form-group">
+								<label for="exampleInputEmail1">State</label>
+								<select name="state" id="state" class="form-control">
+									<option value="AL">Alabama</option>
+									<option value="AK">Alaska</option>
+									<option value="AZ">Arizona</option>
+									<option value="AR">Arkansas</option>
+									<option value="CA">California</option>
+									<option value="CO">Colorado</option>
+									<option value="CT">Connecticut</option>
+									<option value="DE">Delaware</option>
+									<option value="DC">District Of Columbia</option>
+									<option value="FL">Florida</option>
+									<option value="GA">Georgia</option>
+									<option value="HI">Hawaii</option>
+									<option value="ID">Idaho</option>
+									<option value="IL">Illinois</option>
+									<option value="IN">Indiana</option>
+									<option value="IA">Iowa</option>
+									<option value="KS">Kansas</option>
+									<option value="KY">Kentucky</option>
+									<option value="LA">Louisiana</option>
+									<option value="ME">Maine</option>
+									<option value="MD">Maryland</option>
+									<option value="MA">Massachusetts</option>
+									<option value="MI">Michigan</option>
+									<option value="MN">Minnesota</option>
+									<option value="MS">Mississippi</option>
+									<option value="MO">Missouri</option>
+									<option value="MT">Montana</option>
+									<option value="NE">Nebraska</option>
+									<option value="NV">Nevada</option>
+									<option value="NH">New Hampshire</option>
+									<option value="NJ">New Jersey</option>
+									<option value="NM">New Mexico</option>
+									<option value="NY">New York</option>
+									<option value="NC">North Carolina</option>
+									<option value="ND">North Dakota</option>
+									<option value="OH">Ohio</option>
+									<option value="OK">Oklahoma</option>
+									<option value="OR">Oregon</option>
+									<option value="PA">Pennsylvania</option>
+									<option value="RI">Rhode Island</option>
+									<option value="SC">South Carolina</option>
+									<option value="SD">South Dakota</option>
+									<option value="TN">Tennessee</option>
+									<option value="TX">Texas</option>
+									<option value="UT">Utah</option>
+									<option value="VT">Vermont</option>
+									<option value="VA">Virginia</option>
+									<option value="WA">Washington</option>
+									<option value="WV">West Virginia</option>
+									<option value="WI">Wisconsin</option>
+									<option value="WY">Wyoming</option>
+								</select>
+								<?php
+								if (isset($customerData['state'])) {
+									?>
+									<script>$("#state").val("<?php echo $customerData['state']; ?>");</script>
+									<?php
+								}
+								?>
+							</fieldset>
+							<fieldset class="form-group">
+								<label for="exampleInputEmail1">Zipcode</label>
+								<input type="text" class="form-control" name="zip" placeholder="Your Zipcode" value="<?php echo $customerData['zip']; ?>">
+							</fieldset>
+						</div></div>
+				</div></div>
+
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<span class="text-center center-block">
+					<img src="images/credit-only.png">
+						</span>
+					<fieldset class="form-group col-lg-8">
+						<label for="exampleInputEmail1">Card Number</label>
+						<input type="text" class="form-control" name="cardNumber" placeholder="Your Card Number" value="<?php echo $customerData['cardNumber']; ?>">
+					</fieldset>
+					<fieldset class="form-group col-lg-4">
+						<label for="exampleInputEmail1">Card CVV</label>
+						<input type="text" class="form-control" name="cardCVV" placeholder="CVV (On Back)" value="<?php echo $customerData['cardCVV']; ?>">
+					</fieldset>
+					<fieldset class="form-group col-lg-6">
+						<label for="exampleInputEmail1">Expiration Month</label>
+						<select name="expirationMonth" id="expirationMonth" class="form-control">
+							<option value="01">January</option>
+							<option value="02">February</option>
+							<option value="03">March</option>
+							<option value="04">April</option>
+							<option value="05">May</option>
+							<option value="06">June</option>
+							<option value="07">July</option>
+							<option value="08">August</option>
+							<option value="09">September</option>
+							<option value="10">October</option>
+							<option value="11">November</option>
+							<option value="12">December</option>
+						</select>
+						<?php
+						if (isset($customerData['expirationMonth'])) {
+							?>
+							<script>$("#expirationMonth").val("<?php echo $customerData['expirationMonth']; ?>");</script>
+							<?php
+						}
+						?>
+					</fieldset>
+					<fieldset class="form-group col-lg-6">
+						<label for="exampleInputEmail1">Expiration Year</label>
+						<select name="expirationYear" id="expirationYear" class="form-control">
+							<option value="2016">2016</option>
+							<option value="2017">2017</option>
+							<option value="2018">2018</option>
+							<option value="2019">2019</option>
+							<option value="2020">2020</option>
+							<option value="2021">2021</option>
+							<option value="2022">2022</option>
+							<option value="2023">2023</option>
+							<option value="2024">2024</option>
+							<option value="2025">2025</option>
+							<option value="2026">2026</option>
+						</select>
+						<?php
+						if (isset($customerData['expirationYear'])) {
+							?>
+							<script>$("#expirationYear").val("<?php echo $customerData['expirationYear']; ?>");</script>
+							<?php
+						}
+						?>
+					</fieldset>
+				</div>
+			</div>
+			<button type="button" class="btn btn-success btn-lg btn-block">YES! Ship my XT808 NOW!</button>
+
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<p>
+				Copyright © Primitive Survivor 2016. All Rights Reserved.			</p>
+		</div>
+	</div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	<script src="js/validator.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
