7e3a01d510e7ccc12e1424681e388b023e12901e
Bill <bill@tumedia.net>
2016-06-13 11:15:58 -0400

    #TM-12 : increase PageSpeed Insights's mobile speed 80

---
+++ js/_index.js
@@ -0,0 +1,44 @@
+$(document).ready(function()
+{
+	var bModal = true;
+
+	$(document).mousemove(function (e)
+	{
+		if (e.pageY <= 5 && bModal)
+		{
+			$("#popup").modal();
+		}
+	});
+
+	$(".fullName").change(function ()
+	{   //console.log($(this).val());
+		consumerName = getFirstLast($(".fullName").val());
+		//tempArray = consumerName.split(" ");
+		$(".firstName").val(consumerName[0]);
+		$(".lastName").val(consumerName[1]);
+	});
+
+	$(".kform_submitBtn").click(function ()
+	{
+		consumerName = $(".fullName").val();
+		tempArray = consumerName.split(" ");
+
+		$(".firstName").val(tempArray[0]);
+		$(".lastName").val(tempArray[1]);
+		$(".kform").submit();
+	});
+
+	$("#submitButton").click(function (e)
+	{
+		$(".kform").submit();
+	});
+
+	$('.form-control').keypress(function (e)
+	{
+		if (e.which == 13)
+		{
+			console.log("\n pressed enter");
+			$(".kform").submit();
+		}
+	});
+});
