784eb3e8f29f7925f6e5591ca5e5a69869b78372
Bill <bill@tumedia.net>
2016-06-22 23:23:32 -0400

    #TM-23 : Not translate for English

--- js/locale.js
+++ js/locale.js
@@ -4,30 +4,32 @@ $(document).ready(function() {
     if (locale != 'es') {
         locale = 'en';
     }
-    $.getJSON("locale/" + locale + "/" + pageInfo.langFile + ".json", function (json) {
-        $.each($("[trans]"), function (index, item) {
-            var trans = $(item).text();
-            if (json[trans] != undefined) {
-                $(item).text(json[trans]);
+    if (locale != 'en') {
+        $.getJSON("locale/" + locale + "/" + pageInfo.langFile + ".json", function (json) {
+            $.each($("[trans]"), function (index, item) {
+                var trans = $(item).text();
+                if (json[trans] != undefined) {
+                    $(item).text(json[trans]);
+                }
+            });
+
+            $.each($("[placeholder]"), function (index, item) {
+                var trans = $(item).attr('placeholder');
+                if (json[trans] != undefined) {
+                    $(item).attr('placeholder', json[trans]);
+                }
+            });
+
+            $.each($("trans"), function (index, item) {
+                var trans = $(item).text();
+                if (json[trans] != undefined) {
+                    $(item).text(json[trans]);
+                }
+            });
+
+            if (pageInfo.langFile == 'recharge') {
+                $("button#upsellNo").text(json['no_thanks_i_will_just_use_some']);
             }
         });
-
-        $.each($("[placeholder]"), function (index, item) {
-            var trans = $(item).attr('placeholder');
-            if (json[trans] != undefined) {
-                $(item).attr('placeholder', json[trans]);
-            }
-        });
-
-        $.each($("trans"), function (index, item) {
-            var trans = $(item).text();
-            if (json[trans] != undefined) {
-                $(item).text(json[trans]);
-            }
-        });
-
-        if (pageInfo.langFile == 'recharge') {
-            $("button#upsellNo").text(json['no_thanks_i_will_just_use_some']);
-        }
-    });
+    }
 });
