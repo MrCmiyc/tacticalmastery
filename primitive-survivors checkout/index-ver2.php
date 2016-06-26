ca3f6a25dbfbf3d40ec6fe4414d9c570486eb1e5
Ivan Tomicic <ivan@tumedia.net>
2016-06-26 14:01:03 +0200

    push for bill

---
+++ primitive-survivors checkout/index-ver2.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <title>Primitive Survovors</title>
+
+
+        <link rel="stylesheet" type="text/css" href="styles/css/stylesheet.css">
+
+    </head>
+    <body>
+        <div class="overhide"> <!-- Start: Hide elements overlaping page -->
+
+
+                <!-- Start: Page Content -->
+
+                    <div class="container">
+                        <header class="logo-header">
+                            <img src="images/logo-prim.png" alt="Primitive Survivors">
+                        </header>
+
+                        <div class="top-banner">
+                            <p class="title"><span>XT</span>808 Tactical <span class="blink">Flashlight</span></p>
+
+                            <div class="bottom">
+                                <div class="image">
+                                    <img src="images/flashlight.png" alt="XT808 Tactical Flashlight">
+                                </div>
+
+                                <ul class="table">
+                                    <li class="half">Material: Aluminium</li>
+                                    <li class="half">Power: 3XAAA</li>
+                                    <li class="half">Lumens: 800</li>
+                                    <li class="half">Zoom: 1x-2000x</li>
+                                    <li class="full">5 Modes: High | Medium | Low | Strobe | SOS</li>
+                                    <li class="full">Normally: <span class="strike">$116.00</span></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <p class="page-message">Today, you qualify for free shipping! Order Now!</p>
+
+                        <form action="#" class="order-form">
+                            <div class="column">
+                                <p class="section-title">Step #1: Select Quantity</p>
+
+                                <table class="select-table">
+                                    <thead>
+                                        <tr>
+                                            <th>Item</th>
+                                            <th></th>
+                                            <th>Price</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                          <td class="radio"><input type="radio" name="productId" id="3lights" value="5"></td>
+                                          <td class="label"><label for="3lights"><strong>Buy 3 Primitive Survivor Flashlights, GET 2 FREE!</strong> ($29/EACH) <span id="txtblink" style="color:#F00; font-weight:bold;">BEST VALUE</span></label></td>
+                                          <td class="price">$145.00</td>
+                                        </tr>
+                                        <tr>
+                                          <td class="radio"><input type="radio" name="productId" value="4" id="2lights"></td>
+                                          <td class="label"><label for="2lights"><strong>Buy 2 Primitive Survivor Flashlights, GET 1 FREE!</strong> ($39/EACH)</label></td>
+                                          <td class="price">$117.00</td>
+                                        </tr>
+                                        <tr>
+                                          <td class="radio"><input type="radio" name="productId" value="3" id="1light"></td>
+                                          <td class="label"><label for="1light">1 Primitive Survivor Flashlight ($56.00)</label></td>
+                                          <td class="price">$56.00</td>
+                                        </tr>
+                                        <tr>
+                                          <td class="radio"><input type="radio" name="productId" value="6" id="4lights"></td>
+                                          <td class="label"><label for="4lights">2 Primitive Survivor Flashlights ($48.50 Per Unit)</label></td>
+                                          <td class="price">$97.00</td>
+                                        </tr>
+                                        <tr>
+                                          <td class="radio"><input type="radio" name="productId" value="7" id="5lights"></td>
+                                          <td class="label"><label for="5lights">4 Primitive Survivor Flashlights ($42.25 Per Unit)</label></td>
+                                          <td class="price">$169.00</td>
+                                        </tr>
+                                        <tr>
+                                          <td class="radio"><input type="radio" name="productId" value="8" id="10lights"></td>
+                                          <td class="label"><label for="10lights">10 Primitive Survivor Flashlights ($35.00 Per Unit)</label></td>
+                                          <td class="price">$350.00</td>
+                                        </tr>
+                                        <tr>
+                                          <td class="radio"><input type="radio" name="productId" value="9" id="15lights"></td>
+                                          <td class="label"><label for="15lights">15 Primitive Survivor Flashlights ($35.00 Per Unit)</label></td>
+                                          <td class="price">$525</td>
+                                        </tr>
+                                        <tr>
+                                          <td class="radio"><input type="radio" name="productId" value="10" id="20lights"></td>
+                                          <td class="label"><label for="20lights">20 Primitive Survivor Flashlights ($35.00 Per Unit)</label></td>
+                                          <td class="price">$700</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+
+                                <div class="discount-label">
+                                    <p class="huge">75% Off</p>
+                                    <p class="smaller">your discount has been applied</p>
+                                </div>
+
+                                <p class="simple-text">YOUR FREE SHIPPING HAS BEEN APPLIED</p>
+
+                                <p class="section-title">Step #2: Contact Information</p>
+
+                                <div class="input-field">
+                                    <label for="#">First Name:</label>
+                                    <input type="text" placeholder="First Name">
+                                </div>
+
+                                <div class="input-field">
+                                    <label for="#">Last Name:</label>
+                                    <input type="text" placeholder="Last Name">
+                                </div>
+
+                                <div class="input-field">
+                                    <label for="#">Phone Number:</label>
+                                    <input type="text" placeholder="Phone Number">
+                                </div>
+
+                                <div class="input-field">
+                                    <label for="#">Email:</label>
+                                    <input type="text" placeholder="Email">
+                                </div>
+
+                            </div>
+
+                            <div class="column">
+                                
+
+                                <p class="section-title">Step #3: Shipping Address</p>
+
+                                <div class="input-field">
+                                    <label for="#">Street Address:</label>
+                                    <input type="text" placeholder="Your Address">
+                                </div>
+
+                                <div class="input-field">
+                                    <label for="#">City:</label>
+                                    <input type="text" placeholder="Your City">
+                                </div>
+
+                                <div class="input-field">
+                                    <label for="#">State:</label>
+                                    <select name="state" id="state" class="form-control">
+                                        <option value="">Select Your State</option>
+                                        <option value="AL">Alabama</option>
+                                        <option value="AK">Alaska</option>
+                                        <option value="AZ">Arizona</option>
+                                        <option value="AR">Arkansas</option>
+                                        <option value="CA">California</option>
+                                        <option value="CO">Colorado</option>
+                                        <option value="CT">Connecticut</option>
+                                        <option value="DE">Delaware</option>
+                                        <option value="DC">District Of Columbia</option>
+                                        <option value="FL">Florida</option>
+                                        <option value="GA">Georgia</option>
+                                        <option value="HI">Hawaii</option>
+                                        <option value="ID">Idaho</option>
+                                        <option value="IL">Illinois</option>
+                                        <option value="IN">Indiana</option>
+                                        <option value="IA">Iowa</option>
+                                        <option value="KS">Kansas</option>
+                                        <option value="KY">Kentucky</option>
+                                        <option value="LA">Louisiana</option>
+                                        <option value="ME">Maine</option>
+                                        <option value="MD">Maryland</option>
+                                        <option value="MA">Massachusetts</option>
+                                        <option value="MI">Michigan</option>
+                                        <option value="MN">Minnesota</option>
+                                        <option value="MS">Mississippi</option>
+                                        <option value="MO">Missouri</option>
+                                        <option value="MT">Montana</option>
+                                        <option value="NE">Nebraska</option>
+                                        <option value="NV">Nevada</option>
+                                        <option value="NH">New Hampshire</option>
+                                        <option value="NJ">New Jersey</option>
+                                        <option value="NM">New Mexico</option>
+                                        <option value="NY">New York</option>
+                                        <option value="NC">North Carolina</option>
+                                        <option value="ND">North Dakota</option>
+                                        <option value="OH">Ohio</option>
+                                        <option value="OK">Oklahoma</option>
+                                        <option value="OR">Oregon</option>
+                                        <option value="PA">Pennsylvania</option>
+                                        <option value="RI">Rhode Island</option>
+                                        <option value="SC">South Carolina</option>
+                                        <option value="SD">South Dakota</option>
+                                        <option value="TN">Tennessee</option>
+                                        <option value="TX">Texas</option>
+                                        <option value="UT">Utah</option>
+                                        <option value="VT">Vermont</option>
+                                        <option value="VA">Virginia</option>
+                                        <option value="WA">Washington</option>
+                                        <option value="WV">West Virginia</option>
+                                        <option value="WI">Wisconsin</option>
+                                        <option value="WY">Wyoming</option>
+                                    </select>
+                                </div>
+
+                                <div class="input-field">
+                                    <label for="#">Zipcode:</label>
+                                    <input type="text" placeholder="Your Zipcode">
+                                </div>
+
+                                <div class="image-container">
+                                    <img src="images/flashlight.png" alt="Flashlight">
+                                </div>
+
+                                <p class="simple-text">Free Shipping Applied</p>
+
+                                <p class="section-title">Step #4: Payment information</p>
+
+                                <ul class="accepted-cards card_logos">
+                                    <li class="card_visa"><img src="images/cc_visa.jpg" alt="Visa"></li>
+                                    <li class="card_mastercard"><img src="images/cc_master.jpg" alt="Master"></li>
+                                    <li class="card_amex"><img src="images/cc_amex.jpg" alt="Amex"></li>
+                                    <li class="card_discover"><img src="images/cc_discover.jpg" alt="Discover"></li>
+                                    <!-- <li class="card_diners"> ICON NEEDED / OPTIONAL LINE </li> -->
+                                    <!-- <li class="card_jcb"> ICON NEEDED / OPTIONAL LINE </li> -->
+                                </ul>
+
+                                <div class="input-field two-thirds">
+                                    <label for="#">Card Number:</label>
+                                    <input id="checkout_card_number" type="text" size="20" pattern="[0-9]*" autocomplete="off" class="stripe_card_number" required="required" />
+                                </div>
+
+                                <div class="input-field third">
+                                    <label for="#">Card CVV:</label>
+                                    <input type="text" placeholder="CVV">
+                                </div>
+
+                                <div class="input-field half">
+                                    <label for="#">Expiration Month:</label>
+                                    <select name="expirationMonth" id="expirationMonth" class="form-control">
+                                        <option value="#" disabled selected>Month</option>
+                                        <option value="01">January</option>
+                                        <option value="02">February</option>
+                                        <option value="03">March</option>
+                                        <option value="04">April</option>
+                                        <option value="05">May</option>
+                                        <option value="06">June</option>
+                                        <option value="07">July</option>
+                                        <option value="08">August</option>
+                                        <option value="09">September</option>
+                                        <option value="10">October</option>
+                                        <option value="11">November</option>
+                                        <option value="12">December</option>
+                                    </select>
+                                </div>
+
+                                <div class="input-field half">
+                                    <label for="#">Expiration Year:</label>
+                                    <select name="expirationYear" id="expirationYear" class="form-control">
+                                        <option value="#" disabled selected>Year</option>
+                                        <option value="2016">2016</option>
+                                        <option value="2017">2017</option>
+                                        <option value="2018">2018</option>
+                                        <option value="2019">2019</option>
+                                        <option value="2020">2020</option>
+                                        <option value="2021">2021</option>
+                                        <option value="2022">2022</option>
+                                        <option value="2023">2023</option>
+                                        <option value="2024">2024</option>
+                                        <option value="2025">2025</option>
+                                        <option value="2026">2026</option>
+                                    </select>
+                                </div>
+
+                                <div class="submit-field">
+                                    <button type="submit">Ship My XT808 Now</button>
+                                </div>
+
+                            </div>
+                        </form>
+
+                        <p class="copyright">Copyright © Primitive Survivor 2016. All Rights Reserved.</p>
+
+
+
+
+
+                    </div>
+
+                <!-- End: Page Content -->
+        
+        </div> <!-- End: Hide elements overlaping page -->
+
+        <!-- Start: Javascript -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+            <script src="scripts/min/creditCardTypeDetector.js"></script>
+
+            <script>
+                $(document).ready(function(){
+                    $('#checkout_card_number').creditCardTypeDetector({ 'credit_card_logos' : '.card_logos' });
+                });
+            </script>
+
+        <!-- End: Javascript -->
+
+    </body>
+</html>
