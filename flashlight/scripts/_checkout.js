0be177ccb4f0804b99e6024f77ca92ed91d6bdd1
Brian Veverka <brian@veverka.me>
2016-06-30 02:50:22 -0400

    everything shoudl be on our server meow

---
+++ flashlight/scripts/_checkout.js
@@ -0,0 +1,18 @@
+var isBack = true;
+
+window.onbeforeunload = function (e) {
+    if (isBack == true) {
+        return "Are you sure to leave?";
+    }
+};
+
+function validate() {
+    isBack = false;
+    return true;
+}
+
+$(document).ready(function() {
+    $("a.a-prevent-back").click(function() {
+        isBack = false;
+    });
+});
